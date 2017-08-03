5decd162af28e4d85859785d3b89107e2b82ca39
Nikolay Ivanov <nikolai.p.ivanov@gmail.com>
2017-08-04 00:06:38 +0300

    delegate class improved and size reduced. Now should be on par with std::function

--- engine/core/signals/delegate.hpp
+++ engine/core/signals/delegate.hpp
@@ -2,54 +2,326 @@
 #define DELEGATE_HPP
 #pragma once
 
+#include <assert.h>
 #include <cstring>
 #include <memory>
+
 template <typename T>
 class delegate;
 template <class R, class... A>
 class delegate<R(A...)>
 {
-	using stub_ptr_type = R (*)(void*, A&&...);
 
-	delegate(void* const o, stub_ptr_type const m) noexcept
-		: object_ptr_(o)
-		, stub_ptr_(m)
+	template <class C>
+	using member_pair = ::std::pair<C* const, R (C::*const)(A...)>;
+
+	template <class C>
+	using const_member_pair = ::std::pair<C const* const, R (C::*const)(A...) const>;
+
+	template <typename>
+	struct is_member_pair : ::std::false_type
 	{
-	}
+	};
+
+	template <class C>
+	struct is_member_pair<::std::pair<C* const, R (C::*const)(A...)>> : ::std::true_type
+	{
+	};
+
+	template <typename>
+	struct is_const_member_pair : ::std::false_type
+	{
+	};
+
+	template <class C>
+	struct is_const_member_pair<::std::pair<C const* const, R (C::*const)(A...) const>> : ::std::true_type
+	{
+	};
+
+	template <typename F>
+	struct is_location_invariant : std::is_trivially_copyable<F>::type
+	{
+	};
+
+	class undefined_class;
+
+	struct pairtype
+	{
+		undefined_class* object;
+		void (undefined_class::*member_pointer)();
+	};
+
+	union nocopy_types {
+		void* object;
+		const void* const_object;
+		void (*function_pointer)();
+		void (undefined_class::*member_pointer)();
+		pairtype pair;
+	};
+
+	union any_data {
+		void* access()
+		{
+			return &pod_data[0];
+		}
+		const void* access() const
+		{
+			return &pod_data[0];
+		}
+
+		template <typename F>
+		F& access()
+		{
+			return *static_cast<F*>(access());
+		}
+
+		template <typename F>
+		const F& access() const
+		{
+			return *static_cast<const F*>(access());
+		}
+
+		nocopy_types unused;
+		char pod_data[sizeof(nocopy_types)];
+	};
+
+	enum manager_operation
+	{
+		get_functor_ptr,
+		clone_functor,
+		destroy_functor,
+		compare_functor,
+	};
+
+	// Simple type wrapper that helps avoid annoying const problems
+	// when casting between void pointers and pointers-to-pointers.
+	template <typename F>
+	struct simple_type_wrapper
+	{
+		simple_type_wrapper(F value)
+			: _value(value)
+		{
+		}
+
+		F _value;
+	};
+
+	template <typename F>
+	struct is_location_invariant<simple_type_wrapper<F>> : is_location_invariant<F>
+	{
+	};
+
+	constexpr static const std::size_t max_size = sizeof(nocopy_types);
+	constexpr static const std::size_t max_align = alignof(nocopy_types);
+
+	template <typename F>
+	class base_manager
+	{
+	protected:
+		constexpr static const bool stored_locally =
+			(is_location_invariant<F>::value && sizeof(F) <= max_size && alignof(F) <= max_align &&
+			 (max_align % alignof(F) == 0));
+
+		using local_storage = std::integral_constant<bool, stored_locally>;
+		using is_copiable = typename std::is_copy_constructible<F>::type;
+		using is_movable = typename std::is_move_constructible<F>::type;
+		using is_any_member_pair =
+			std::integral_constant<bool, is_const_member_pair<F>::value || is_member_pair<F>::value>;
+
+		using is_function_pointer =
+			std::integral_constant<bool,
+								   std::is_pointer<F>::value &&
+									   std::is_function<typename std::remove_pointer<F>::type>::value>;
+
+		// Retrieve a pointer to the function object
+		static F* get_pointer(const any_data& source)
+		{
+			const F* ptr = stored_locally ? std::__addressof(source.template access<F>())
+										  /* have stored a pointer */
+										  : source.template access<F*>();
+			return const_cast<F*>(ptr);
+		}
+
+		// Clone a location-invariant function object that fits within
+		// an any_data structure.
+		static void clone(any_data& dest, const any_data& source, std::true_type, std::true_type)
+		{
+			new(dest.access()) F(source.template access<F>());
+		}
+
+		// Clone a function object that is not location-invariant or
+		// that cannot fit into an any_data structure.
+		static void clone(any_data& dest, const any_data& source, std::true_type, std::false_type)
+		{
+			dest.template access<F*>() = new F(*source.template access<F*>());
+		}
+
+		static void clone(any_data&, const any_data&, std::false_type, std::true_type)
+		{
+			assert(false && "TRYING TO COPY A NON-COPYABLE FUNCTOR/FUNCTION");
+		}
+
+		// Clone a function object that is not location-invariant or
+		// that cannot fit into an any_data structure.
+		static void clone(any_data&, const any_data&, std::false_type, std::false_type)
+		{
+			assert(false && "TRYING TO COPY A NON-COPYABLE FUNCTOR/FUNCTION");
+		}
+
+		// Compare a function object that is a member pair.
+		static bool compare_impl_pair(const any_data& data1, const any_data& data2)
+		{
+			const F* pair1 = get_pointer(data1);
+			const auto& obj_ptr1 = pair1->first;
+			const auto& func_ptr1 = pair1->second;
+
+			const F* pair2 = get_pointer(data2);
+			const auto& obj_ptr2 = pair2->first;
+			const auto& func_ptr2 = pair2->second;
+
+			return obj_ptr1 == obj_ptr2 && func_ptr1 == func_ptr2;
+		}
+
+		static bool compare_impl_function(const any_data& data1, const any_data& data2, std::true_type)
+		{
+			const F* ptr1 = get_pointer(data1);
+			const F* ptr2 = get_pointer(data2);
+
+			return *ptr1 == *ptr2;
+		}
+
+		static bool compare_impl_function(const any_data& data1, const any_data& data2, std::false_type)
+		{
+			const F* ptr1 = get_pointer(data1);
+			const F* ptr2 = get_pointer(data2);
+
+			return ptr1 == ptr2;
+		}
+
+		static bool compare(const any_data& data1, const any_data& data2, std::true_type)
+		{
+			return compare_impl_pair(data1, data2);
+		}
+
+		static bool compare(const any_data& data1, const any_data& data2, std::false_type)
+		{
+			return compare_impl_function(data1, data2, is_function_pointer());
+		}
+
+		// Destroying a location-invariant object may still require
+		// destruction.
+		static void destroy(any_data& victim, std::true_type)
+		{
+			victim.template access<F>().~F();
+		}
+
+		// Destroying an object located on the heap.
+		static void destroy(any_data& victim, std::false_type)
+		{
+			delete victim.template access<F*>();
+		}
+
+	public:
+		static void* manager(any_data& dest, const any_data& dest_const, const any_data& source,
+							 manager_operation op)
+		{
+			switch(op)
+			{
+				case get_functor_ptr:
+					dest.template access<F*>() = get_pointer(source);
+					return get_pointer(source);
+
+				case compare_functor:
+					if(compare(dest_const, source, is_any_member_pair()))
+					{
+						return get_pointer(source);
+					}
+					return nullptr;
+				case clone_functor:
+					clone(dest, source, is_copiable(), local_storage());
+					return get_pointer(dest);
+
+				case destroy_functor:
+					destroy(dest, local_storage());
+					return get_pointer(dest);
+			}
+			return nullptr;
+		}
+
+		static void initF(any_data& _F, F&& f)
+		{
+			initF(_F, std::move(f), local_storage());
+		}
+
+		template <typename _Signature>
+		static bool not_empty_function(const delegate<_Signature>& f)
+		{
+			return static_cast<bool>(f);
+		}
+
+		template <typename Func>
+		static bool not_empty_function(Func* const& fp)
+		{
+			return fp;
+		}
+
+		template <typename _Class, typename Func>
+		static bool not_empty_function(Func _Class::*const& mp)
+		{
+			return mp;
+		}
+
+		template <typename Func>
+		static bool not_empty_function(const Func&)
+		{
+			return true;
+		}
+
+	private:
+		static void initF(any_data& _F, F&& f, std::true_type)
+		{
+			new(_F.access()) F(std::move(f));
+		}
+
+		static void initF(any_data& _F, F&& f, std::false_type)
+		{
+			_F.template access<F*>() = new F(std::move(f));
+		}
+	};
+
+	using manager_type = void* (*)(any_data&, const any_data&, const any_data&, manager_operation);
+	using invoker_type = R (*)(void* const, A&&...);
+
+	any_data functor_;
+	manager_type manager_ = nullptr;
+	invoker_type invoker_ = nullptr;
 
 public:
-	delegate(void) = default;
+	delegate() = default;
 
-	delegate(delegate const&) = default;
+	~delegate()
+	{
+		if(manager_)
+			manager_(functor_, functor_, functor_, destroy_functor);
+	}
+
+	delegate(delegate const& rhs)
+	{
+		if(static_cast<bool>(rhs))
+		{
+			rhs.manager_(functor_, functor_, rhs.functor_, clone_functor);
+			invoker_ = rhs.invoker_;
+			manager_ = rhs.manager_;
+		}
+	}
 
 	delegate(delegate&& d)
-		: object_ptr_(d.object_ptr_)
-		, stub_ptr_(d.stub_ptr_)
-		, deleter_(d.deleter_)
-		, store_(d.store_)
-		, store_size_(d.store_size_)
 	{
-		d.object_ptr_ = nullptr;
-		d.stub_ptr_ = nullptr;
-		d.deleter_ = nullptr;
-		d.store_ = nullptr;
-		d.store_size_ = 0;
+		d.swap(*this);
 	}
 
 	delegate(::std::nullptr_t const) noexcept
 		: delegate()
-	{
-	}
-
-	template <class C, typename = typename ::std::enable_if<::std::is_class<C>::value, C>::type>
-	explicit delegate(C const* const o) noexcept
-		: object_ptr_(const_cast<C*>(o))
-	{
-	}
-
-	template <class C, typename = typename ::std::enable_if<::std::is_class<C>::value, C>::type>
-	explicit delegate(C const& o) noexcept
-		: object_ptr_(const_cast<C*>(&o))
 	{
 	}
 
@@ -81,48 +353,54 @@ public:
 			  typename = typename ::std::enable_if<
 				  !::std::is_same<delegate, typename ::std::decay<T>::type>::value>::type>
 	delegate(T&& f)
-		: store_(operator new(sizeof(typename ::std::decay<T>::type)),
-				 functor_deleter<typename ::std::decay<T>::type>)
-		, store_size_(sizeof(typename ::std::decay<T>::type))
 	{
+		using handler = base_manager<typename std::decay<T>::type>;
 		using functor_type = typename ::std::decay<T>::type;
 
-		new(store_.get()) functor_type(::std::forward<T>(f));
-		object_ptr_ = store_.get();
+		if(handler::not_empty_function(f))
+		{
+			handler::initF(functor_, std::move(f));
 
-		stub_ptr_ = functor_stub<functor_type>;
-		deleter_ = deleter_stub<functor_type>;
+			invoker_ = &functor_stub<functor_type>;
+			manager_ = &handler::manager;
+		}
 	}
 
-	delegate& operator=(delegate const&) = default;
+	delegate& operator=(delegate const& d)
+	{
+		delegate(d).swap(*this);
+		return *this;
+	}
 
 	delegate& operator=(delegate&& d)
 	{
-		object_ptr_ = d.object_ptr_;
-		stub_ptr_ = d.stub_ptr_;
-		deleter_ = d.deleter_;
-		store_ = d.store_;
-		store_size_ = d.store_size_;
+		delegate(std::move(d)).swap(*this);
+		return *this;
+	}
 
-		d.object_ptr_ = nullptr;
-		d.stub_ptr_ = nullptr;
-		d.deleter_ = nullptr;
-		d.store_ = nullptr;
-		d.store_size_ = 0;
-
+	delegate& operator=(std::nullptr_t)
+	{
+		if(manager_)
+		{
+			manager_(functor_, functor_, functor_, destroy_functor);
+			manager_ = nullptr;
+			invoker_ = nullptr;
+		}
 		return *this;
 	}
 
 	template <class C>
 	delegate& operator=(R (C::*const rhs)(A...))
 	{
-		return *this = from(static_cast<C*>(object_ptr_), rhs);
+		const void* object_ptr = get_object_ptr();
+		return *this = from(static_cast<C*>(object_ptr), rhs);
 	}
 
 	template <class C>
 	delegate& operator=(R (C::*const rhs)(A...) const)
 	{
-		return *this = from(static_cast<C const*>(object_ptr_), rhs);
+		const void* object_ptr = get_object_ptr();
+		return *this = from(static_cast<C const*>(object_ptr), rhs);
 	}
 
 	template <typename T,
@@ -130,21 +408,7 @@ public:
 				  !::std::is_same<delegate, typename ::std::decay<T>::type>::value>::type>
 	delegate& operator=(T&& f)
 	{
-		using functor_type = typename ::std::decay<T>::type;
-
-		if((sizeof(functor_type) > store_size_) || !store_.unique())
-		{
-			store_.reset(operator new(sizeof(functor_type)), functor_deleter<functor_type>);
-			store_size_ = sizeof(functor_type);
-		}
-		else
-			deleter_(store_.get());
-
-		new(store_.get()) functor_type(::std::forward<T>(f));
-		object_ptr_ = store_.get();
-
-		stub_ptr_ = functor_stub<functor_type>;
-		deleter_ = deleter_stub<functor_type>;
+		delegate(std::forward<T>(f)).swap(*this);
 
 		return *this;
 	}
@@ -191,12 +455,6 @@ public:
 	}
 
 	template <class C>
-	using member_pair = ::std::pair<C* const, R (C::*const)(A...)>;
-
-	template <class C>
-	using const_member_pair = ::std::pair<C const* const, R (C::*const)(A...) const>;
-
-	template <class C>
 	static delegate from(C* const object_ptr, R (C::*const method_ptr)(A...))
 	{
 		return member_pair<C>(object_ptr, method_ptr);
@@ -220,30 +478,20 @@ public:
 		return const_member_pair<C>(&object, method_ptr);
 	}
 
-	void reset(void)
-	{
-		stub_ptr_ = nullptr;
-		store_.reset();
-	}
-
-	void reset_stub(void) noexcept
-	{
-		stub_ptr_ = nullptr;
-	}
-
 	void swap(delegate& other) noexcept
 	{
-		::std::swap(*this, other);
+		std::swap(functor_, other.functor_);
+		std::swap(manager_, other.manager_);
+		std::swap(invoker_, other.invoker_);
 	}
 
 	bool operator==(delegate const& rhs) const noexcept
 	{
-		// comparison between functor and non-functor is left as undefined at the
-		// moment.
-		if(store_size_ && rhs.store_size_) // both functors
-			return (std::memcmp(store_.get(), rhs.store_.get(), store_size_) == 0) &&
-				   (stub_ptr_ == rhs.stub_ptr_);
-		return (object_ptr_ == rhs.object_ptr_) && (stub_ptr_ == rhs.stub_ptr_);
+		any_data dest;
+		if(manager_(dest, functor_, rhs.functor_, compare_functor))
+			return true;
+
+		return invoker_ == rhs.invoker_;
 	}
 
 	bool operator!=(delegate const& rhs) const noexcept
@@ -253,56 +501,46 @@ public:
 
 	bool operator<(delegate const& rhs) const noexcept
 	{
-		return (object_ptr_ < rhs.object_ptr_) ||
-			   ((object_ptr_ == rhs.object_ptr_) && (stub_ptr_ < rhs.stub_ptr_));
+		const void* object_ptr = get_object_ptr();
+		const void* rhs_object_ptr = rhs.get_object_ptr();
+
+		return (object_ptr < rhs_object_ptr) || ((object_ptr == rhs_object_ptr) && (invoker_ < rhs.invoker_));
 	}
 
 	bool operator==(::std::nullptr_t const) const noexcept
 	{
-		return !stub_ptr_;
+		return !invoker_;
 	}
 
 	bool operator!=(::std::nullptr_t const) const noexcept
 	{
-		return stub_ptr_;
+		return invoker_;
 	}
 
 	explicit operator bool() const noexcept
 	{
-		return !!stub_ptr_;
+		return !!invoker_;
 	}
 
 	R operator()(A... args) const
 	{
-		//  assert(stub_ptr);
-		return stub_ptr_(object_ptr_, ::std::forward<A>(args)...);
+		void* object_ptr = get_object_ptr();
+		assert(object_ptr);
+		return invoker_(object_ptr, ::std::forward<A>(args)...);
 	}
 
 private:
+	void* get_object_ptr() const
+	{
+		if(manager_)
+		{
+			any_data data;
+			return manager_(data, functor_, functor_, get_functor_ptr);
+		}
+		return nullptr;
+	}
+
 	friend struct ::std::hash<delegate>;
-
-	using deleter_type = void (*)(void*);
-
-	void* object_ptr_ = nullptr;
-	stub_ptr_type stub_ptr_{};
-
-	deleter_type deleter_ = nullptr;
-
-	::std::shared_ptr<void> store_ = nullptr;
-	::std::size_t store_size_ = 0;
-
-	template <class T>
-	static void functor_deleter(void* const p)
-	{
-		static_cast<T*>(p)->~T();
-		operator delete(p);
-	}
-
-	template <class T>
-	static void deleter_stub(void* const p)
-	{
-		static_cast<T*>(p)->~T();
-	}
 
 	template <R (*function_ptr)(A...)>
 	static R function_stub(void* const, A&&... args)
@@ -321,26 +559,6 @@ private:
 	{
 		return (static_cast<C const*>(object_ptr)->*method_ptr)(::std::forward<A>(args)...);
 	}
-
-	template <typename>
-	struct is_member_pair : ::std::false_type
-	{
-	};
-
-	template <class C>
-	struct is_member_pair<::std::pair<C* const, R (C::*const)(A...)>> : ::std::true_type
-	{
-	};
-
-	template <typename>
-	struct is_const_member_pair : ::std::false_type
-	{
-	};
-
-	template <class C>
-	struct is_const_member_pair<::std::pair<C const* const, R (C::*const)(A...) const>> : ::std::true_type
-	{
-	};
 
 	template <typename T>
 	static typename ::std::enable_if<!(is_member_pair<T>::value || is_const_member_pair<T>::value), R>::type
@@ -366,7 +584,7 @@ struct hash<delegate<R(A...)>>
 	size_t operator()(::delegate<R(A...)> const& d) const noexcept
 	{
 		auto const seed(hash<void*>()(d.object_ptr_));
-		return hash<decltype(d.stub_ptr_)>()(d.stub_ptr_) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+		return hash<decltype(d.invoker_)>()(d.invoker_) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 	}
 };
 } // namespace std
