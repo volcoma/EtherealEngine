f106c33e7c5e3d9c3cc405b1548080c7a7bc342c
volcoma <nikolai.p.ivanov@gmail.com>
2018-12-29 01:58:45 +0200

    updated bgfx cmake

--- 3rdparty/bgfx/cmake/3rdparty/pvrtc.cmake
+++ 3rdparty/bgfx/cmake/3rdparty/pvrtc.cmake
@@ -12,8 +12,8 @@ if( TARGET pvrtc )
 	return()
 endif()
 
-file( GLOB SQUISH_SOURCES ${BIMG_DIR}/3rdparty/pvrtc/*.cpp ${BIMG_DIR}/3rdparty/pvrtc/*.h )
+file( GLOB PVRTC_SOURCES ${BIMG_DIR}/3rdparty/pvrtc/*.cpp ${BIMG_DIR}/3rdparty/pvrtc/*.h )
 
-add_library( pvrtc STATIC ${SQUISH_SOURCES} )
+add_library( pvrtc STATIC ${PVRTC_SOURCES} )
 target_include_directories( pvrtc PUBLIC ${BIMG_DIR}/3rdparty )
 set_target_properties( pvrtc PROPERTIES FOLDER "bgfx/3rdparty" )
